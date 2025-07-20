15b7f164d8a673e5c688b9bbb489ff48ae4e4c49
goheungchoi <89227091+goheungchoi@users.noreply.github.com>
2025-07-20 21:56:02 +0900

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+﻿// GameAnimationProgramming.cpp : Defines the entry point for the application.
+//
+
+#include "GameAnimationProgramming.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
